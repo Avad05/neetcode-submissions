71f00f552aa5235f2801121d1a252403348d20da
Avad05 <167281581+Avad05@users.noreply.github.com>
2026-09-07 14:21:10 +0530

    Add: merge-k-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-4.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+
+        int interval = 1;
+
+        while (interval < lists.size()) {
+            for (int i = 0; i + interval < lists.size(); i += interval * 2) {
+                lists[i] = merge(lists[i], lists[i + interval]);
+            }
+
+            interval *= 2;
+        }
+
+        return lists[0];
+    }
+
+private:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+
+            curr = curr->next;
+        }
+
+        if (l1) curr->next = l1;
+        else curr->next = l2;
+
+        return dummy.next;
+    }
+};
